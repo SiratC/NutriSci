b56bed8c39a7d929b744f89d4b9e4fbbdf0cc28e
Sirat <127055475+SiratC@users.noreply.github.com>
2025-06-15 02:55:49 -0400

    Update NutrientAnalyzer.java

--- NutriSci/src/main/java/org/Handlers/Logic/NutrientAnalyzer.java
+++ NutriSci/src/main/java/org/Handlers/Logic/NutrientAnalyzer.java
@@ -1,4 +1,21 @@
 package org.Handlers.Logic;
 
-public class NutrientAnalyzer {
+
+import org.Entity.Meal;
+import org.Entity.NutrientStats;
+
+import java.util.List;
+
+public class NutrientAnalyzer implements Analyzer<List<Meal>, NutrientStats> {
+    @Override
+    public NutrientStats analyze(List<Meal> meals) {
+        int totalItems = 0;
+        for (Meal m : meals) {
+            totalItems += m.getItems().size();
+        }
+        NutrientStats stats = new NutrientStats();
+        stats.setTotalItems(totalItems);
+        // TODO
+        return stats;
+    }
 }
