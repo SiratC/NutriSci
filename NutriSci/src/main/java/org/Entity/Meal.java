6717dd982aff3170507e5ecdaf09754d06afae29
Sirat <127055475+SiratC@users.noreply.github.com>
2025-06-15 02:53:22 -0400

    Update Meal.java

--- NutriSci/src/main/java/org/Entity/Meal.java
+++ NutriSci/src/main/java/org/Entity/Meal.java
@@ -1,10 +1,32 @@
 package org.Entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Meal {
-    private long id;
-    private LocalDate date;
-    private List<Food> items;
+    private final UUID id = UUID.randomUUID();
+    private final LocalDate date;
+    private final List<Food> items = new ArrayList<>();
+
+    public Meal(LocalDate date) {
+        this.date = date;
+    }
+
+    public void addItem(Food food) {
+        items.add(food);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public List<Food> getItems() {
+        return items;
+    }
 }
