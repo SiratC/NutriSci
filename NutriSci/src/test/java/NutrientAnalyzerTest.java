17e174f5d3b77592ec24147af90c5538df4ea891
Sooryakiran Ponnath <sooryakiranponnath@gmail.com>
2025-06-18 15:33:07 -0400

    tweak test names

--- NutriSci/src/test/java/NutrientAnalyzerTest.java
+++ NutriSci/src/test/java/NutrientAnalyzerTest.java
@@ -7,21 +7,20 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
 
 public class NutrientAnalyzerTest {
     @Test
-    void test1() { // no item
+    void getTotalItemsTest1() { // no item
         NutrientAnalyzer na = new NutrientAnalyzer();
-        assertEquals(0, na.analyze(List.of()).getTotalItems());
+        Assertions.assertEquals(0, na.analyze(List.of()).getTotalItems());
     }
 
     @Test
-    void test2() { // with 1 added item
+    void getTotalItemsTest2() { // with 1 added item
         Meal m = new Meal(LocalDate.now());
         m.addItem(new Food("Egg", 2));
         NutrientAnalyzer na = new NutrientAnalyzer();
-        assertEquals(1, na.analyze(List.of(m)).getTotalItems());
-
+        Assertions.assertEquals(1, na.analyze(List.of(m)).getTotalItems());
     }
 }
