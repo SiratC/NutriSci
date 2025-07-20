a04df4e85d6bc4eaee4ff606b07797a4af9be597
mhassen2332 <mariamhassen2332@gmail.com>
2025-07-20 18:47:06 -0400

    Update TrendAnalyzer.java

--- NutriSci/src/main/java/org/Handlers/Logic/TrendAnalyzer.java
+++ NutriSci/src/main/java/org/Handlers/Logic/TrendAnalyzer.java
@@ -1,4 +1,5 @@
 package org.Handlers.Logic;
+
 import org.Entity.*;
 import org.Enums.NutrientType;
 import java.time.LocalDate;
@@ -12,47 +13,37 @@ public class TrendAnalyzer implements Analyzer<List<Meal>, TrendResult> {
     public TrendResult analyze(List<Meal> meals) {
 
         Map<LocalDate, NutrientStats> perDayStats = new TreeMap<>();
-
         Map<NutrientType, Double> totalMap = new EnumMap<>(NutrientType.class);
-
-        // group by date
         Map<LocalDate, List<Meal>> byDate = new HashMap<>();
 
         for (Meal meal : meals) {
-
             byDate.computeIfAbsent(meal.getDate(), d -> new ArrayList<>()).add(meal);
         }
 
-        // computing stats for each date
-        for (Map.Entry<LocalDate, List<Meal>> entry : byDate.entrySet()) {
+        NutrientStats.NutrientStatsTemplate template = new NutrientStats.Top3Template();
 
+        for (Map.Entry<LocalDate, List<Meal>> entry : byDate.entrySet()) {
             LocalDate date = entry.getKey();
             List<Meal> dayMeals = entry.getValue();
 
             Map<NutrientType, Double> dailyMap = new EnumMap<>(NutrientType.class);
 
             for (Meal meal : dayMeals) {
-
                 Map<NutrientType, Double> mealMap = calculator.calculate(meal);
                 for (Map.Entry<NutrientType, Double> m : mealMap.entrySet()) {
-
                     dailyMap.merge(m.getKey(), m.getValue(), Double::sum);
-                    totalMap.merge(m.getKey(), m.getValue(), Double::sum); // for cumulative
+                    totalMap.merge(m.getKey(), m.getValue(), Double::sum);
                 }
             }
 
-            NutrientStats dailyStats = new NutrientStats();
-            dailyStats.setStats(dailyMap);
+            NutrientStats dailyStats = template.calculateStats(dailyMap);
             perDayStats.put(date, dailyStats);
         }
 
-        // creating result
         TrendResult result = new TrendResult();
-
         List<NutrientStats> perMealStats = new ArrayList<>(perDayStats.values());
 
-        NutrientStats cumulative = new NutrientStats();
-        cumulative.setStats(totalMap);
+        NutrientStats cumulative = template.calculateStats(totalMap);
         result.setPerMealStats(perMealStats);
         result.setCumulativeStats(cumulative);
 
