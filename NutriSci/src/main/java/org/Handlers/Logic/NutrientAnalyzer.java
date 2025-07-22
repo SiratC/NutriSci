6e1cd27dd79ac37b933a861a267a288ee188d244
Sirat <127055475+SiratC@users.noreply.github.com>
2025-07-22 17:08:17 -0400

    Update NutrientAnalyzer.java

--- NutriSci/src/main/java/org/Handlers/Logic/NutrientAnalyzer.java
+++ NutriSci/src/main/java/org/Handlers/Logic/NutrientAnalyzer.java
@@ -6,6 +6,7 @@ import org.Enums.NutrientType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class NutrientAnalyzer implements Analyzer<List<Meal>, NutrientStats> {
 
@@ -13,33 +14,32 @@ public class NutrientAnalyzer implements Analyzer<List<Meal>, NutrientStats> {
 
     @Override
     public NutrientStats analyze(List<Meal> meals) {
-
         Map<NutrientType, Double> nutrientSums = new HashMap<>();
         int totalItems = 0;
 
         for (Meal meal : meals) {
-
             for (Food food : meal.getItems()) {
-
                 totalItems++;
-                Map<NutrientType, Double> foodNutrients = lookup.getPerUnit(food.getFoodID());
+                Map<NutrientType, Double> foodNutrients = food.getNutrients();
 
                 if (foodNutrients != null) {
-
                     for (Map.Entry<NutrientType, Double> entry : foodNutrients.entrySet()) {
-
-                        nutrientSums.merge(entry.getKey(), entry.getValue() * food.getQuantity(), Double::sum);
+                        nutrientSums.merge(entry.getKey(), entry.getValue(), Double::sum);
                     }
                 }
             }
         }
 
         NutrientStats.NutrientStatsTemplate template = new NutrientStats.Top3Template();
-
         NutrientStats stats = template.calculateStats(nutrientSums);
-
         stats.setTotalItems(totalItems);
 
         return stats;
     }
+
+    public void update(String action, UUID userId, List<Meal> meals) {
+        // observer-compatible method
+        System.out.println("[NutrientAnalyzer] update triggered: " + action + " for user " + userId);
+    }
 }
+
